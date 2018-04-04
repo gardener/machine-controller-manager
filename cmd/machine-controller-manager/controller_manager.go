887d534389f479c718f2c9deda6cf88630149943
Bozhin Katsraski <bozhin@gmail.com>
2018-04-04 16:36:32 +0300

    Added liveliness probe (/healthz)  - This is a combination of 4 commits.

--- cmd/machine-controller-manager/controller_manager.go
+++ cmd/machine-controller-manager/controller_manager.go
@@ -31,16 +31,11 @@ import (
 	_ "github.com/gardener/machine-controller-manager/pkg/util/reflector/prometheus"      // for reflector metric registration
 	_ "github.com/gardener/machine-controller-manager/pkg/util/workqueue/prometheus"      // for workqueue metric registration
 	"github.com/spf13/pflag"
-	"k8s.io/apiserver/pkg/server/healthz"
 	"k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 	// TODO version should be enabled later on. DON'T import k8s.io/kubernetes 	_ "k8s.io/kubernetes/pkg/version/prometheus"        // for version metric registration
 	// TODO version should be enabled later on. DON'T import k8s.io/kubernetes  "k8s.io/kubernetes/pkg/version/verflag"
 )
-
-func init() {
-	healthz.DefaultHealthz()
-}
 
 func main() {
 
