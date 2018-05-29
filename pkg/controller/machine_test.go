1950e4d99d3644bb23dee49e60b27ffe2fe9b2e9
PadmaB <padmashree.b@sap.com>
2018-05-29 15:54:42 +0530

    Unit Testcase in machine.go for checking health of the machine

--- pkg/controller/machine_test.go
+++ pkg/controller/machine_test.go
@@ -21,7 +21,9 @@ import (
 	machinev1 "github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	fakemachineapi "github.com/gardener/machine-controller-manager/pkg/client/clientset/versioned/typed/machine/v1alpha1/fake"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	k8stesting "k8s.io/client-go/testing"
@@ -33,12 +35,13 @@ var _ = Describe("machine", func() {
 		c                 *controller
 	)
 
-	Describe("##updateMachineStatus", func() {
+	Describe("#updateMachineStatus", func() {
 		var (
 			machine       *machinev1.Machine
 			lastOperation machinev1.LastOperation
 			currentStatus machinev1.CurrentStatus
 		)
+
 		BeforeEach(func() {
 			fakeMachineClient = &fakemachineapi.FakeMachineV1alpha1{
 				Fake: &k8stesting.Fake{},
@@ -52,14 +55,12 @@ var _ = Describe("machine", func() {
 					Namespace: "test",
 				},
 			}
-
 			lastOperation = machinev1.LastOperation{
 				Description:    "test operation",
 				LastUpdateTime: metav1.Now(),
 				State:          machinev1.MachineStateProcessing,
 				Type:           "Create",
 			}
-
 			currentStatus = machinev1.CurrentStatus{
 				LastUpdateTime: lastOperation.LastUpdateTime,
 				Phase:          machinev1.MachinePending,
@@ -103,12 +104,14 @@ var _ = Describe("machine", func() {
 				}
 
 				return false, nil, nil
+
 			})
 
 			machineRet, errRet := c.updateMachineStatus(machine, lastOperation, currentStatus)
 			Expect(errRet).Should(BeNil())
 			Expect(machineRet).Should(Not(BeNil()))
 			Expect(machineUpdated).Should(Not(BeNil()))
+
 			Expect(machineUpdated).Should(Not(BeIdenticalTo(machine)))
 			Expect(machineRet).Should(Not(BeIdenticalTo(machine)))
 			Expect(machineRet).Should(BeIdenticalTo(machineUpdated))
@@ -116,4 +119,78 @@ var _ = Describe("machine", func() {
 			Expect(machineRet.Status.LastOperation).Should(BeIdenticalTo(lastOperation))
 		})
 	})
+
+	Describe("#isHealthy", func() {
+		BeforeEach(func() {
+			fakeMachineClient = &fakemachineapi.FakeMachineV1alpha1{
+				Fake: &k8stesting.Fake{},
+			}
+			c = &controller{
+				controlMachineClient: fakeMachineClient,
+			}
+		})
+
+		testMachine := machinev1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "testmachine",
+				Namespace: "test",
+			},
+			Status: machinev1.MachineStatus{
+				Conditions: []corev1.NodeCondition{},
+			},
+		}
+		DescribeTable("Checking health of the machine",
+			func(conditionType corev1.NodeConditionType, conditionStatus corev1.ConditionStatus, expected bool) {
+				testMachine.Status.Conditions = []corev1.NodeCondition{
+					{
+						Type:   corev1.NodeReady,
+						Status: corev1.ConditionTrue,
+					},
+					{
+						Type:   corev1.NodeOutOfDisk,
+						Status: corev1.ConditionFalse,
+					},
+					{
+						Type:   corev1.NodeDiskPressure,
+						Status: corev1.ConditionFalse,
+					},
+					{
+						Type:   corev1.NodeMemoryPressure,
+						Status: corev1.ConditionFalse,
+					},
+					{
+						Type:   corev1.NodeNetworkUnavailable,
+						Status: corev1.ConditionFalse,
+					},
+					{
+						Type:   corev1.NodeConfigOK,
+						Status: corev1.ConditionTrue,
+					},
+				}
+				for i, condition := range testMachine.Status.Conditions {
+					if condition.Type == conditionType {
+						testMachine.Status.Conditions[i].Status = conditionStatus
+						break
+					}
+				}
+				Expect(c.isHealthy(&testMachine)).Should(BeIdenticalTo(expected))
+			},
+			Entry("with NodeReady is True", corev1.NodeReady, corev1.ConditionTrue, true),
+			Entry("with NodeReady is False", corev1.NodeReady, corev1.ConditionFalse, false),
+			Entry("with NodeReady is Unknown", corev1.NodeReady, corev1.ConditionUnknown, false),
+			Entry("with NodeOutOfDisk is True", corev1.NodeOutOfDisk, corev1.ConditionTrue, false),
+			Entry("with NodeOutOfDisk is Unknown", corev1.NodeOutOfDisk, corev1.ConditionUnknown, false),
+			Entry("with NodeDiskPressure is True", corev1.NodeDiskPressure, corev1.ConditionTrue, false),
+			Entry("with NodeDiskPressure is Unknown", corev1.NodeDiskPressure, corev1.ConditionUnknown, false),
+			Entry("with NodeMemoryPressure is True", corev1.NodeMemoryPressure, corev1.ConditionTrue, false),
+			Entry("with NodeMemoryPressure is Unknown", corev1.NodeMemoryPressure, corev1.ConditionUnknown, false),
+			Entry("with NodeNetworkUnavailable is True", corev1.NodeNetworkUnavailable, corev1.ConditionTrue, false),
+			Entry("with NodeNetworkUnavailable is Unknown", corev1.NodeNetworkUnavailable, corev1.ConditionUnknown, false),
+
+			//TODO: To be enhanced later, currently unclear on how to handle the NodeConfigOK NodeConditionType
+			//Entry("with NodeConfigOK is False", corev1.NodeConfigOK, corev1.ConditionFalse, false),
+			//Entry("with NodeConfigOK is Unknown", corev1.NodeConfigOK, corev1.ConditionUnknown, false),
+		)
+	})
+
 })
