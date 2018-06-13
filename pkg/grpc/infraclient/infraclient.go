f3e0f7ce064f424f9d583aed329031adc8a89829
Gupta <gaurav.gupta07@sap.com>
2018-06-13 14:35:34 +0530

    Correction in license header

--- pkg/grpc/infraclient/infraclient.go
+++ pkg/grpc/infraclient/infraclient.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 SAP SE or an SAP affiliate company. All rights reserved.
+Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
