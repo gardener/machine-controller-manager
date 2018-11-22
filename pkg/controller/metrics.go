88bece55c3c63d223178435e425244f37fc6b434
Onur Yalazı <oyalazi@ptc.com>
2018-11-22 16:38:22 +0000

    Added mcm_[machineset|machinedeployments]_items_total, mcm_machine_created

--- pkg/controller/metrics.go
+++ pkg/controller/metrics.go
@@ -18,12 +18,21 @@ limitations under the License.
 package controller
 
 import (
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/apimachinery/pkg/labels"
 )
 
 var (
-	machineCountDesc = prometheus.NewDesc("mcm_machine_items_total", "Count of machines currently managed by the mcm.", nil, nil)
+	machineCountDesc           = prometheus.NewDesc("mcm_machine_items_total", "Count of machines currently managed by the mcm.", nil, nil)
+	machineSetCountDesc        = prometheus.NewDesc("mcm_machineset_items_total", "Count of machinesets currently managed by the mcm.", nil, nil)
+	machineDeploymentCountDesc = prometheus.NewDesc("mcm_machinedeployment_items_total", "Count of machinedeployments currently managed by the mcm.", nil, nil)
+
+	MachineCreated = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mcm_machine_created",
+		Help: "Creation time of machines currently managed by the mcm.",
+	}, []string{"Name", "Namespace", "UID", "Generation"})
 
 	// ScrapeFailedCounter is a Prometheus metric, which counts errors during metrics collection.
 	ScrapeFailedCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
@@ -34,6 +43,7 @@ var (
 
 func init() {
 	prometheus.MustRegister(ScrapeFailedCounter)
+	prometheus.MustRegister(MachineCreated)
 }
 
 // Describe is method required to implement the prometheus.Collect interface.
@@ -49,9 +59,48 @@ func (c *controller) Collect(ch chan<- prometheus.Metric) {
 		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machine-count"}).Inc()
 		return
 	}
+
+	for _, machine := range machineList {
+		meta := machine.ObjectMeta
+		MachineCreated.With(prometheus.Labels{
+			"Name":       meta.Name,
+			"Namespace":  meta.Namespace,
+			"UID":        string(meta.UID),
+			"Generation": strconv.FormatInt(meta.Generation, 16)}).Set(
+			float64(meta.GetCreationTimestamp().Time.Unix()))
+		if err != nil {
+			ScrapeFailedCounter.With(prometheus.Labels{"kind": "machine"}).Inc()
+			return
+		}
+	}
+
 	metric, err := prometheus.NewConstMetric(machineCountDesc, prometheus.GaugeValue, float64(len(machineList)))
 	if err != nil {
 		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machine-count"}).Inc()
+		return
+	}
+	ch <- metric
+
+	machineSetList, err := c.machineSetLister.MachineSets(c.namespace).List(labels.Everything())
+	if err != nil {
+		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machineset-count"}).Inc()
+		return
+	}
+	metric, err = prometheus.NewConstMetric(machineSetCountDesc, prometheus.GaugeValue, float64(len(machineSetList)))
+	if err != nil {
+		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machineset-count"}).Inc()
+		return
+	}
+	ch <- metric
+
+	machineDeploymentList, err := c.machineDeploymentLister.MachineDeployments(c.namespace).List(labels.Everything())
+	if err != nil {
+		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machinedeployment-count"}).Inc()
+		return
+	}
+	metric, err = prometheus.NewConstMetric(machineDeploymentCountDesc, prometheus.GaugeValue, float64(len(machineDeploymentList)))
+	if err != nil {
+		ScrapeFailedCounter.With(prometheus.Labels{"kind": "machinedeployment-count"}).Inc()
 		return
 	}
 	ch <- metric
