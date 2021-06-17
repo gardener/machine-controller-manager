53bc18f40fad31f581adb22bd53e19c0bece776d
prashanth26 <prashanth@sap.com>
2021-06-17 23:28:24 +0530

    Avoids blocking of drain call
    - There was a deadlock condition where the buffer is full and no more requests could be processed. This change fixes it.
    - It also makes sure to deleteWorker in failure/negative cases

--- pkg/util/provider/drain/volume_attachment.go
+++ pkg/util/provider/drain/volume_attachment.go
@@ -52,13 +52,19 @@ func (v *VolumeAttachmentHandler) dispatch(obj interface{}) {
 	}
 
 	klog.V(4).Infof("Dispatching request for PV %s", *volumeAttachment.Spec.Source.PersistentVolumeName)
+	defer klog.V(4).Infof("Done dispatching request for PV %s", *volumeAttachment.Spec.Source.PersistentVolumeName)
 
 	v.Lock()
 	defer v.Unlock()
 
 	for i, worker := range v.workers {
-		klog.V(4).Infof("Dispatching request for PV %s to worker %d", *volumeAttachment.Spec.Source.PersistentVolumeName, i)
-		worker <- volumeAttachment
+		klog.V(4).Infof("Dispatching request for PV %s to worker %d/%v", *volumeAttachment.Spec.Source.PersistentVolumeName, i, worker)
+
+		select {
+		case worker <- volumeAttachment:
+		default:
+			klog.Warningf("Worker %d/%v is full. Discarding value.", i, worker)
+		}
 	}
 }
 
@@ -78,9 +84,10 @@ func (v *VolumeAttachmentHandler) UpdateVolumeAttachment(oldObj, newObj interfac
 func (v *VolumeAttachmentHandler) AddWorker() chan *storagev1.VolumeAttachment {
 	// chanSize is the channel buffer size to hold requests.
 	// This assumes that not more than 20 unprocessed objects would exist at a given time.
+	// On bufferring requests beyond this the channel will start dropping writes
 	const chanSize = 20
 
-	klog.V(4).Infof("Adding new worker. Current active workers %d", len(v.workers))
+	klog.V(4).Infof("Adding new worker. Current active workers %d - %v", len(v.workers), v.workers)
 
 	v.Lock()
 	defer v.Unlock()
@@ -88,13 +95,13 @@ func (v *VolumeAttachmentHandler) AddWorker() chan *storagev1.VolumeAttachment {
 	newWorker := make(chan *storagev1.VolumeAttachment, chanSize)
 	v.workers = append(v.workers, newWorker)
 
-	klog.V(4).Infof("Successfully added new worker %v. Current active workers %d", newWorker, len(v.workers))
+	klog.V(4).Infof("Successfully added new worker %v. Current active workers %d - %v", newWorker, len(v.workers), v.workers)
 	return newWorker
 }
 
 // DeleteWorker is the method used to delete an existing worker
 func (v *VolumeAttachmentHandler) DeleteWorker(desiredWorker chan *storagev1.VolumeAttachment) {
-	klog.V(4).Infof("Deleting an existing worker %v. Current active workers %d", desiredWorker, len(v.workers))
+	klog.V(4).Infof("Deleting an existing worker %v. Current active workers %d - %v", desiredWorker, len(v.workers), v.workers)
 
 	v.Lock()
 	defer v.Unlock()
@@ -111,5 +118,5 @@ func (v *VolumeAttachmentHandler) DeleteWorker(desiredWorker chan *storagev1.Vol
 	}
 
 	v.workers = finalWorkers
-	klog.V(4).Infof("Successfully removed worker. Current active workers %d", len(v.workers))
+	klog.V(4).Infof("Successfully removed worker. Current active workers %d - %v", len(v.workers), v.workers)
 }
