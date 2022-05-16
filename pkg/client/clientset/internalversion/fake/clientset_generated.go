51bbdc5e0d399127ee7c8e55e7385dbb72d73f95
Mayank Mittal <mayank.mittal@sap.com>
2022-05-16 16:35:01 +0530

    Api Violation correction

--- pkg/client/clientset/internalversion/fake/clientset_generated.go
+++ pkg/client/clientset/internalversion/fake/clientset_generated.go
@@ -74,7 +74,10 @@ func (c *Clientset) Tracker() testing.ObjectTracker {
 	return c.tracker
 }
 
-var _ clientset.Interface = &Clientset{}
+var (
+	_ clientset.Interface = &Clientset{}
+	_ testing.FakeClient  = &Clientset{}
+)
 
 // Machine retrieves the MachineClient
 func (c *Clientset) Machine() machineinternalversion.MachineInterface {
