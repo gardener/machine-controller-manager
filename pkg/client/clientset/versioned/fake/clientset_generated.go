67522498edc0333e4f858fbd00744100d2da5544
Mayank Mittal <39642031+Mkmittal@users.noreply.github.com>
2022-05-27 14:18:55 +0530

    Upgrade k8s dependencies to v1.22.9(the latest patch) (#721)
    * Api Violation correction done

--- pkg/client/clientset/versioned/fake/clientset_generated.go
+++ pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -74,7 +74,10 @@ func (c *Clientset) Tracker() testing.ObjectTracker {
 	return c.tracker
 }
 
-var _ clientset.Interface = &Clientset{}
+var (
+	_ clientset.Interface = &Clientset{}
+	_ testing.FakeClient  = &Clientset{}
+)
 
 // MachineV1alpha1 retrieves the MachineV1alpha1Client
 func (c *Clientset) MachineV1alpha1() machinev1alpha1.MachineV1alpha1Interface {
