5ef1a6aadc70b7be1b94167b661cd0bff2110fbd
Ismail Alidzhikov <i.alidjikov@gmail.com>
2023-03-14 12:45:01 +0200

    Fix the API docs generation (#800)
    Co-authored-by: himanshu-kun <himanshu.sharma10@sap.com>

--- hack/api-reference/generate-spec-doc.sh
+++ hack/api-reference/generate-spec-doc.sh
@@ -1,4 +1,4 @@
 cd ./hack/api-reference
-./gen-crd-api-reference-docs -config "providerspec-config.json" -api-dir "../../pkg/apis/machine/v1alpha1" -out-file="../../docs/docs/apis.md"
-sed 's/?id=//g' ../../docs/docs/apis.md > ../../docs/docs/apis-1.md
-mv ../../docs/docs/apis-1.md ../../docs/docs/apis.md
+./gen-crd-api-reference-docs -config "providerspec-config.json" -api-dir "../../pkg/apis/machine/v1alpha1" -out-file="../../docs/documents/apis.md"
+sed 's/?id=//g' ../../docs/documents/apis.md > ../../docs/documents/apis-1.md
+mv ../../docs/documents/apis-1.md ../../docs/documents/apis.md
