e89b6d6f190eb730b00126d2bd2762a18bcfe0be
Madhav Bhargava <madhav.bhargava@sap.com>
2023-08-29 12:53:49 +0530

    corrected local_setup.sh, added local_restore.sh, added script to add license headers

--- hack/add_license_headers.sh
+++ hack/add_license_headers.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 # Copyright 2023 SAP SE or an SAP affiliate company
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd ./hack/api-reference
-./gen-crd-api-reference-docs -config "providerspec-config.json" -api-dir "../../pkg/apis/machine/v1alpha1" -out-file="../../docs/documents/apis.md"
-sed 's/?id=//g' ../../docs/documents/apis.md > ../../docs/documents/apis-1.md
-mv ../../docs/documents/apis-1.md ../../docs/documents/apis.md
+
+set -e
+
+echo "> Adding Apache License header to all go files where it is not present"
+
+# Uses the tool https://github.com/google/addlicense
+YEAR="$(date +%Y)"
+addlicense \
+  -c "SAP SE or an SAP affiliate company" \
+  -y "${YEAR}" \
+  -l apache \
+  -ignore ".idea/**" \
+  -ignore ".vscode/**" \
+  -ignore "vendor/**" \
+  -ignore "**/*.md" \
+  -ignore "**/*.yaml" \
+  -ignore "**/Dockerfile" \
+  .
