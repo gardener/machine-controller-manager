5f5a727b075929d1977acebce1b6760ab9732fb1
Madhav Bhargava <madhav.bhargava@sap.com>
2023-09-22 16:02:39 +0530

    Updates to local setup scripts and introduce new local setup scripts for non-gardener use cases (#852)
    * wip commit
    
    * created separate script for non-gardener use case and also updated the doc
    
    * added validation for kubeconfig expiry seconds
    
    * addressed review comments
    
    * minor corrections to the local_setup.md
    
    * remove the info print statements from tools.mk

---
+++ hack/gardener_local_restore.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+# Copyright 2023 SAP SE or an SAP affiliate company
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -o errexit
+set -o pipefail
+
+##############################################################################################################
+# Script restores MCM deployment and removes the annotation dependency-watchdog.gardener.cloud/ignore-scaling
+# This script should be called once you are done using gardener_local_setup.sh
+##############################################################################################################
+
+declare SHOOT PROJECT PROVIDER
+declare PROVIDER_MCM_PROJECT_DIR
+declare RELATIVE_KUBECONFIG_PATH="dev/kube-configs"
+declare LANDSCAPE_NAME="dev"
+
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}")" &>/dev/null && pwd)"
+
+function create_usage() {
+  usage=$(printf '%s\n' "
+    Usage: $(basename $0) [Options]
+    Options:
+      -t | --shoot                      <shoot-cluster-name>                (Required) Name of the Gardener Shoot Cluster
+      -p | --project                    <project-name>                      (Required) Name of the Gardener Project
+      -l | --landscape                  <landscape-name>                    (Optional) Name of the landscape. Defaults to dev
+      -i | --provider                   <provider-name>                     (Required) Infrastructure provider name. Supported providers (gcp|aws|azure|vsphere|openstack|alicloud|metal|equinix-metal)
+      -k | --kubeconfig-path            <relative-kubeconfig-path>          (Optional) Relative path to the <PROJECT-DIR> where kubeconfigs will be downloaded. Path should not start with '/'. Default: <PROJECT-DIR>/dev/kube-configs
+      -m | --mcm-provider-project-path  <absolute-mcm-provider-project-dir> (Optional) MCM Provider project directory. If not provided then it assumes that both mcm and mcm-provider projects are under the same parent directory
+    ")
+  echo "${usage}"
+}
+
+function parse_flags() {
+  while test $# -gt 0; do
+    case "$1" in
+    --shoot | -t)
+      shift
+      SHOOT="$1"
+      ;;
+    --project | -p)
+      shift
+      PROJECT="$1"
+      ;;
+    --landscape | -l)
+      shift
+      LANDSCAPE_NAME="$1"
+      ;;
+    --provider | -i)
+      shift
+      PROVIDER="$1"
+      ;;
+    --kubeconfig-path | -k)
+      shift
+      RELATIVE_KUBECONFIG_PATH="$1"
+      ;;
+    --mcm-provider-project-path | -m)
+      shift
+      PROVIDER_MCM_PROJECT_DIR="$1"
+      ;;
+    --help | -h)
+      shift
+      echo "${USAGE}"
+      exit 0
+      ;;
+    esac
+    shift
+  done
+}
+
+function validate_args() {
+  if [[ -z "${SHOOT}" ]]; then
+    echo -e "Shoot has not been passed. Please provide Shoot either by specifying --shoot or -t argument"
+    exit 1
+  fi
+  if [[ -z "${PROJECT}" ]]; then
+    echo -e "Project name has not been passed. Please provide Project name either by specifying --project or -p argument"
+    exit 1
+  fi
+}
+
+function initialize_variables() {
+  if [[ -z "${PROVIDER_MCM_PROJECT_DIR}" ]]; then
+    PROVIDER_MCM_PROJECT_DIR=$(dirname "${PROJECT_DIR}")/machine-controller-manager-provider-"${PROVIDER}"
+  fi
+  ABSOLUTE_KUBE_CONFIG_PATH="$(cd "$(dirname "${RELATIVE_KUBECONFIG_PATH}")"; pwd)/$(basename "${RELATIVE_KUBECONFIG_PATH}")"
+  ABSOLUTE_PROVIDER_KUBECONFIG_PATH="${PROVIDER_MCM_PROJECT_DIR}/dev/kube-configs"
+}
+
+function restore_mcm_deployment() {
+  local virtual_garden_cluster="sap-landscape-${LANDSCAPE_NAME}"
+  gardenctl target --garden "${virtual_garden_cluster}" --project "${PROJECT}" --shoot "${SHOOT}" --control-plane
+  eval "$(gardenctl kubectl-env zsh)"
+  echo "removing annotation dependency-watchdog.gardener.cloud/ignore-scaling from mcm deployment..."
+  kubectl annotate --overwrite=true deployment/machine-controller-manager dependency-watchdog.gardener.cloud/ignore-scaling-
+  echo "scaling up mcm deployment to 1..."
+  kubectl scale deployment/machine-controller-manager --replicas=1
+}
+
+function delete_generated_configs() {
+  echo "Removing downloaded kubeconfigs..."
+  rm -f "${ABSOLUTE_KUBE_CONFIG_PATH}"/*.yaml
+  rm -f "${ABSOLUTE_PROVIDER_KUBECONFIG_PATH}"/*.yaml
+  echo "Clearing .env files..."
+  sed -r -i '/^#/!d' "${PROJECT_DIR}"/.env
+  sed -r -i '/^#/!d' "${PROVIDER_MCM_PROJECT_DIR}"/.env
+  echo "Removing generated admin kube config json if any..."
+  rm -f "${SCRIPT_DIR}"/admin-kube-config-request.json
+}
+
+function main() {
+  parse_flags "$@"
+  validate_args
+  initialize_variables
+  restore_mcm_deployment
+  delete_generated_configs
+}
+
+USAGE=$(create_usage)
+main "$@"
---
+++ hack/non_gardener_local_setup.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+# Copyright 2023 SAP SE or an SAP affiliate company
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o pipefail
+
+##############################################################################################################
+# Script sets up local development environment enabling you to start MCM process locally. This is a non-gardener
+# setup. Therefore it is left to the consumer to ensure that user configured in the kubeconfig has sufficient
+# permissions to modify the machine-controller-manager deployment.
+#
+# It does the following:
+# 1. Ensures that kube-configs are copied to both mcm and provider-mcm project directory
+# 2. Scales down MCM to 0
+# 3. Updates Makefile for mcm and provider-mcm projects and exports variables used by the makefile.
+##############################################################################################################
+
+# these are mandatory cli flags to be provided by the user
+declare PROVIDER NAMESPACE CONTROL_KUBECONFIG_PATH TARGET_KUBECONFIG_PATH
+
+# these are optional cli flags
+declare PROVIDER_MCM_PROJECT_DIR
+declare ABSOLUTE_PROVIDER_KUBECONFIG_PATH
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}")" &>/dev/null && pwd)"
+
+# create_usage creates a CLI usage string.
+function create_usage() {
+  usage=$(printf '%s\n' "
+    Usage: $(basename $0) [Options]
+    Options:
+      -n | --namespace                  <namespace>                         (Required) This is the namespace where MCM pods are deployed.
+      -c | --control-kubeconfig-path    <control-kubeconfig-path>           (Required) Kubeconfig file path which points to the control-plane of cluster where MCM is running.
+      -t | --target-kubeconfig-path     <target-kubeconfig-path>            (Required) Kubeconfig file path which points to control plane of the cluster where nodes are created.
+      -i | --provider                   <provider-name>                     (Required) Infrastructure provider name. Supported providers (gcp|aws|azure|vsphere|openstack|alicloud|metal|equinix-metal)
+      -m | --mcm-provider-project-path  <absolute-mcm-provider-project-dir> (Optional) MCM Provider project directory. If not provided then it assumes that both mcm and mcm-provider projects are under the same parent directory
+    ")
+  echo "${usage}"
+}
+
+# parse_flags parses the CLI arguments passed by the consumer.
+function parse_flags() {
+  while test $# -gt 0; do
+    case "$1" in
+    --namespace | -n)
+      shift
+      NAMESPACE="$1"
+      ;;
+    --control-kubeconfig-path | -c)
+      shift
+      CONTROL_KUBECONFIG_PATH="$1"
+      ;;
+    --target-kubeconfig-path | -t)
+      shift
+      TARGET_KUBECONFIG_PATH="$1"
+      ;;
+    --provider | -i)
+      shift
+      PROVIDER="$1"
+      ;;
+    --mcm-provider-project-path | -m)
+      shift
+      PROVIDER_MCM_PROJECT_DIR="$1"
+      ;;
+    --help | -h)
+      shift
+      echo "${USAGE}"
+      exit 0
+      ;;
+    esac
+    shift
+  done
+}
+
+# validate_args validates the CLI arguments being passed.
+function validate_args() {
+  if [[ -z "${NAMESPACE}" ]]; then
+    echo -e "Namespace is not provided. Please provided it either by specifying --namespace or -n"
+    exit 1
+  fi
+  if [[ -z "${CONTROL_KUBECONFIG_PATH}" ]]; then
+    echo -e "Control Kubeconfig Path is not provided. Please provide it either by specifying --control-kubeconfig-path or -c argument"
+    exit 1
+  fi
+  if [[ ! -f "${CONTROL_KUBECONFIG_PATH}" ]]; then
+    echo -e "File ${CONTROL_KUBECONFIG_PATH} does not exist. Please ensure that the control kubeconfig is present."
+    exit 1
+  fi
+  if [[ -z "${TARGET_KUBECONFIG_PATH}" ]]; then
+    echo -e "Target Kubeconfig Path is not provided. Please provide it either by specifying --target-kubeconfig-path or -t argument"
+    exit 1
+  fi
+  if [[ ! -f "${TARGET_KUBECONFIG_PATH}" ]]; then
+    echo -e "File ${TARGET_KUBECONFIG_PATH} does not exist. Please ensure that the target kubeconfig is present."
+    exit 1
+  fi
+  if [[ -z "${PROVIDER}" ]]; then
+    echo -e "Infrastructure provider name has not been passed. Please provide infrastructure provider name either by specifying --provider or -i argument"
+    exit 1
+  fi
+}
+
+function init_provider_paths() {
+  if [[ -z "${PROVIDER_MCM_PROJECT_DIR}" ]]; then
+    PROVIDER_MCM_PROJECT_DIR=$(dirname "${PROJECT_DIR}")/machine-controller-manager-provider-"${PROVIDER}"
+  fi
+  ABSOLUTE_PROVIDER_KUBECONFIG_PATH="${PROVIDER_MCM_PROJECT_DIR}/dev/kube-configs"
+  echo "Creating directory ${ABSOLUTE_PROVIDER_KUBECONFIG_PATH} if it does not exist..."
+  mkdir -p "${ABSOLUTE_PROVIDER_KUBECONFIG_PATH}"
+}
+
+# copy_kubeconfigs_to_provider_mcm copies the downloaded or manually put kubeconfig files to the MCM provider git repo.
+function copy_kubeconfigs_to_provider_mcm() {
+  cp "${CONTROL_KUBECONFIG_PATH}" "${ABSOLUTE_PROVIDER_KUBECONFIG_PATH}"
+  cp "${TARGET_KUBECONFIG_PATH}" "${ABSOLUTE_PROVIDER_KUBECONFIG_PATH}"
+}
+
+function scale_down_mcm() {
+  echo "scaling down deployment/machine-controller-manager to 0..."
+  KUBECONFIG="${CONTROL_KUBECONFIG_PATH}" kubectl -n "${NAMESPACE}" scale deployment/machine-controller-manager --replicas=0
+}
+
+# create_makefile_env creates a .env file that will get copied to both mcm and mcm-provider project directories for their
+# respective Makefile to use.
+function set_makefile_env() {
+  if [[ $# -ne 1 ]]; then
+    echo -e "${FUNCNAME[0]} expects one argument - project-directory"
+  fi
+
+  local target_project_dir="$1"
+  {
+    printf "\n%s" "CONTROL_NAMESPACE=${NAMESPACE}"
+    printf "\n%s" "CONTROL_KUBECONFIG=${CONTROL_KUBECONFIG_PATH}" >>"${target_project_dir}/.env"
+    printf "\n%s" "TARGET_KUBECONFIG=${TARGET_KUBECONFIG_PATH}" >>"${target_project_dir}/.env"
+  } >>"${target_project_dir}/.env"
+}
+
+# main is the entry point to this script.
+function main() {
+  parse_flags "$@"
+  validate_args
+  init_provider_paths
+  copy_kubeconfigs_to_provider_mcm
+  scale_down_mcm
+  set_makefile_env "${PROJECT_DIR}"
+  set_makefile_env "${PROVIDER_MCM_PROJECT_DIR}"
+}
+
+USAGE=$(create_usage)
+main "$@"
