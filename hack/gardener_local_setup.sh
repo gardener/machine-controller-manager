6695b0099f788a50a0e593697e65ba58b853c84a
thiyyakat <meghana.thiyyakat@gmail.com>
2024-08-20 14:16:53 +0530

    Fix gardener_local_setup script to use correct control cluster namespace (#935)
    * Fixed gardener_local_setup.sh script to use the correct CONTROL_CLUSTER_NAMESPACE
    
    * Update hack/gardener_local_setup.sh
    
    Co-authored-by: Rishabh Patel <66425093+rishabh-11@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rishabh Patel <66425093+rishabh-11@users.noreply.github.com>

--- hack/gardener_local_setup.sh
+++ hack/gardener_local_setup.sh
@@ -198,8 +198,8 @@ function set_makefile_env() {
   target_kube_config_path="$2"
   echo "IS_CONTROL_CLUSTER_SEED=true" > "${target_project_dir}/.env"
   {
-    printf "CONTROL_CLUSTER_NAMESPACE=%s--%s\n" "${PROJECT}" "${SHOOT}";
-    printf "CONTROL_NAMESPACE=%s--%s\n" "${PROJECT}" "${SHOOT}";
+    printf "CONTROL_CLUSTER_NAMESPACE=shoot--%s--%s\n" "${PROJECT}" "${SHOOT}";
+    printf "CONTROL_NAMESPACE=shoot--%s--%s\n" "${PROJECT}" "${SHOOT}";
     printf "CONTROL_KUBECONFIG=%s\n" "${target_kube_config_path}/kubeconfig_control.yaml";
     printf "TARGET_KUBECONFIG=%s\n" "${target_kube_config_path}/kubeconfig_target.yaml";
     printf "LEADER_ELECT=false\n"
