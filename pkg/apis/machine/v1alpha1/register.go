63ca186d0b42ba97039c091f1d2fd3f1649cf3e3
Sonu Kumar Singh <sksgkpvks@gmail.com>
2024-11-19 16:02:16 +0530

    Introduce labels constants for InPlaceUpdate
    Co-Authored-By: Shafeeque E S <shafeeque.e.s@sap.com>
    Co-Authored-By: Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>

--- pkg/apis/machine/v1alpha1/register.go
+++ pkg/apis/machine/v1alpha1/register.go
@@ -20,8 +20,28 @@ var (
 	AddToScheme = SchemeBuilder.AddToScheme
 )
 
-// GroupName is the group name use in this package
-const GroupName = "machine.sapcloud.io"
+const (
+	// GroupName is the group name use in this package
+	GroupName = "machine.sapcloud.io"
+
+	// AnnotationKeyMachineUpdateFailedReason is the annotation key for the machine indicating the reason for the machine update failure.
+	AnnotationKeyMachineUpdateFailedReason = "node.machine.sapcloud.io/update-failed-reason"
+
+	// LabelKeyMachineCandidateForUpdate is the label key for the machine indicating the machine will undergo an update.
+	LabelKeyMachineCandidateForUpdate = "node.machine.sapcloud.io/candidate-for-update"
+	// LabelKeyMachineSelectedForUpdate is the label key for the machine indicating the machine is selected for update.
+	LabelKeyMachineSelectedForUpdate = "node.machine.sapcloud.io/selected-for-update"
+	// LabelKeyMachineReadyForUpdate is the label key for the machine indicating the machine is ready for update after the node drain.
+	LabelKeyMachineReadyForUpdate = "node.machine.sapcloud.io/ready-for-update"
+	// LabelKeyMachineDrainSuccessful is the label key for the machine indicating the node drain was successful.
+	LabelKeyMachineDrainSuccessful = "node.machine.sapcloud.io/drain-successful"
+	// LabelKeyMachineUpdateSuccessful is the label key for the machine indicating the machine update was successful.
+	LabelKeyMachineUpdateSuccessful = "node.machine.sapcloud.io/update-successful"
+	// LabelKeyMachineUpdateFailed is the label key for the machine indicating the machine update failed.
+	LabelKeyMachineUpdateFailed = "node.machine.sapcloud.io/update-failed"
+	// LabelKeyMachineSetSkipUpdate is the label key for the machine indicating the machine set update should be skipped.
+	LabelKeyMachineSetSkipUpdate = "node.machine.sapcloud.io/machine-set-skip-update"
+)
 
 // SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
