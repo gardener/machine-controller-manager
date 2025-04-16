8301ef39bcade8e531c46c998abeedc4f8d13606
Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
2025-04-16 13:17:50 +0530

    Address review comments from @aaronfern.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -12,9 +12,9 @@ import (
 var (
 	// These variables typically come from -ldflags settings in build
 
-	// Version shows the etcd-backup-restore binary version.
+	// Version shows the machine-controller-manager binary version.
 	Version string
-	// GitSHA shows the etcd-backup-restore binary code commit SHA on git.
+	// GitSHA shows the machine-controller-manager binary code commit SHA on git.
 	GitSHA string
 )
 
