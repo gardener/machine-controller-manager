dc78d826918efeb28c7bd17034da4ec5e66a9f06
Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
2025-04-29 11:18:11 +0530

    Fix version logging at binary startup. (#985)
    * Fix version logging at binary startup.
    
    Signed-off-by: Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
    
    * Address review comments from @aaronfern.
    
    * Add `ldflags` to the `start` make target.
    
    ---------
    
    Signed-off-by: Saketh Kalaga <51327242+renormalize@users.noreply.github.com>

---
+++ pkg/version/version.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package version
+
+import (
+	"k8s.io/klog/v2"
+	"runtime"
+)
+
+var (
+	// These variables typically come from -ldflags settings in build
+
+	// Version shows the machine-controller-manager binary version.
+	Version string
+	// GitSHA shows the machine-controller-manager binary code commit SHA on git.
+	GitSHA string
+)
+
+// LogVersionInfoWithLevel logs machine-controller-manager version and build information.
+func LogVersionInfoWithLevel(debugLevel int32) {
+	level := klog.Level(debugLevel)
+	klog.V(level).Infof("machine-controller-manager Version: %s\n", Version)
+	klog.V(level).Infof("Git SHA: %s\n", GitSHA)
+	klog.V(level).Infof("Go Version: %s\n", runtime.Version())
+	klog.V(level).Infof("Go OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+}
