31dac5eb956ad71311ebbe21544d45ee8e3e50ca
Prashant Tak <prashant.tak@sap.com>
2025-07-08 14:44:59 +0530

    Update generate-spec-doc.sh to use absolute paths

--- hack/api-reference/generate-spec-doc.sh
+++ hack/api-reference/generate-spec-doc.sh
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-cd ./hack/api-reference
-../tools/bin/gen-crd-api-reference-docs -config "providerspec-config.json" -api-dir "../../pkg/apis/machine/v1alpha1" -out-file="../../docs/documents/apis.md"
-sed 's/?id=//g' ../../docs/documents/apis.md > ../../docs/documents/apis-1.md
-mv ../../docs/documents/apis-1.md ../../docs/documents/apis.md
+./hack/tools/bin/gen-crd-api-reference-docs -config "./hack/api-reference/providerspec-config.json" -api-dir "./pkg/apis/machine/v1alpha1" -template-dir "./hack/api-reference/template" -out-file="./docs/documents/apis.md"
+sed 's/?id=//g' docs/documents/apis.md > docs/documents/apis-1.md
+mv docs/documents/apis-1.md docs/documents/apis.md
