988018531849fbeb9397da8e7a1f3f07cdb670c7
thiyyakat <meghana.thiyyakat@sap.com>
2026-01-23 10:19:12 +0530

    Address review comments - part 12: - Reuse function to write annotation on machine - Minor refactoring

--- pkg/controller/machineset.go
+++ pkg/controller/machineset.go
@@ -35,19 +35,15 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	errorsutil "k8s.io/apimachinery/pkg/util/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/retry"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/integer"
 
 	"github.com/gardener/machine-controller-manager/pkg/apis/machine"
 	"github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	"github.com/gardener/machine-controller-manager/pkg/apis/machine/validation"
-	v1alpha1client "github.com/gardener/machine-controller-manager/pkg/client/clientset/versioned/typed/machine/v1alpha1"
-	v1alpha1listers "github.com/gardener/machine-controller-manager/pkg/client/listers/machine/v1alpha1"
 	"github.com/gardener/machine-controller-manager/pkg/util/provider/machineutils"
 )
 
@@ -887,38 +883,39 @@ func isMachineStatusEqual(s1, s2 v1alpha1.MachineStatus) bool {
 	return apiequality.Semantic.DeepEqual(s1Copy.LastOperation, s2Copy.LastOperation) && apiequality.Semantic.DeepEqual(s1Copy.CurrentStatus, s2Copy.CurrentStatus)
 }
 
-// see https://github.com/kubernetes/kubernetes/issues/21479
-type updateMachineFunc func(machine *v1alpha1.Machine) error
-
-// UpdateMachineWithRetries updates a machine with given applyUpdate function. Note that machine not found error is ignored.
-// The returned bool value can be used to tell if the machine is actually updated.
-func UpdateMachineWithRetries(ctx context.Context, machineClient v1alpha1client.MachineInterface, machineLister v1alpha1listers.MachineLister, namespace, name string, applyUpdate updateMachineFunc) (*v1alpha1.Machine, error) {
-	var machine *v1alpha1.Machine
-
-	retryErr := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
-		var err error
-		machine, err = machineLister.Machines(namespace).Get(name)
-		if err != nil {
-			return err
-		}
-		machine = machine.DeepCopy()
-		// Apply the update, then attempt to push it to the apiserver.
-		if applyErr := applyUpdate(machine); applyErr != nil {
-			return applyErr
-		}
-		machine, err = machineClient.Update(ctx, machine, metav1.UpdateOptions{})
-		return err
-	})
-
-	// Ignore the precondition violated error, this machine is already updated
-	// with the desired label.
-	if retryErr == errorsutil.ErrPreconditionViolated {
-		klog.V(4).Infof("Machine %s precondition doesn't hold, skip updating it.", name)
-		retryErr = nil
-	}
-
-	return machine, retryErr
-}
+//
+//// see https://github.com/kubernetes/kubernetes/issues/21479
+//type updateMachineFunc func(machine *v1alpha1.Machine) error
+//
+//// UpdateMachineWithRetries updates a machine with given applyUpdate function. Note that machine not found error is ignored.
+//// The returned bool value can be used to tell if the machine is actually updated.
+//func UpdateMachineWithRetries(ctx context.Context, machineClient v1alpha1client.MachineInterface, machineLister v1alpha1listers.MachineLister, namespace, name string, applyUpdate updateMachineFunc) (*v1alpha1.Machine, error) {
+//	var machine *v1alpha1.Machine
+//
+//	retryErr := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+//		var err error
+//		machine, err = machineLister.Machines(namespace).Get(name)
+//		if err != nil {
+//			return err
+//		}
+//		machine = machine.DeepCopy()
+//		// Apply the update, then attempt to push it to the apiserver.
+//		if applyErr := applyUpdate(machine); applyErr != nil {
+//			return applyErr
+//		}
+//		machine, err = machineClient.Update(ctx, machine, metav1.UpdateOptions{})
+//		return err
+//	})
+//
+//	// Ignore the precondition violated error, this machine is already updated
+//	// with the desired label.
+//	if retryErr == errorsutil.ErrPreconditionViolated {
+//		klog.V(4).Infof("Machine %s precondition doesn't hold, skip updating it.", name)
+//		retryErr = nil
+//	}
+//
+//	return machine, retryErr
+//}
 
 // shouldFailedMachineBeTerminated checks if the failed machine is already preserved, in the process of being preserved
 // or if it is a candidate for auto-preservation. If none of these conditions are met, it returns true indicating
@@ -963,32 +960,31 @@ func (c *controller) triggerAutoPreservationOfFailedMachines(ctx context.Context
 			if autoPreservationCapacityRemaining == 0 {
 				return
 			}
-
 			klog.V(2).Infof("Annotating failed machine %q for auto-preservation as part of machine set %q", m.Name, machineSet.Name)
-			err := c.annotateMachineForAutoPreservation(ctx, m)
+			_, err := machineutils.AnnotateMachineWithPreserveValueWithRetries(ctx, c.controlMachineClient.Machines(m.Namespace), c.machineLister, m, machineutils.PreserveMachineAnnotationValuePreservedByMCM)
 			if err != nil {
 				klog.V(2).Infof("Error annotating machine %q for auto-preservation: %v", m.Name, err)
-				// since annotateForAutoPreservation uses retries internally, we can continue with other machines
+				// since AnnotateMachineWithPreserveValueWithRetries uses retries internally, we can continue with other machines
 				continue
 			}
-			autoPreservationCapacityRemaining = autoPreservationCapacityRemaining - 1
+			autoPreservationCapacityRemaining--
 		}
 	}
 }
 
-// annotateMachineForAutoPreservation annotates the given machine with the auto-preservation annotation to trigger
-// preservation of the machine by the machine controller.
-func (c *controller) annotateMachineForAutoPreservation(ctx context.Context, m *v1alpha1.Machine) error {
-	_, err := UpdateMachineWithRetries(ctx, c.controlMachineClient.Machines(m.Namespace), c.machineLister, m.Namespace, m.Name, func(clone *v1alpha1.Machine) error {
-		if clone.Annotations == nil {
-			clone.Annotations = make(map[string]string)
-		}
-		clone.Annotations[machineutils.PreserveMachineAnnotationKey] = machineutils.PreserveMachineAnnotationValuePreservedByMCM
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-	klog.V(2).Infof("Updated machine %q with %q=%q.", m.Name, machineutils.PreserveMachineAnnotationKey, machineutils.PreserveMachineAnnotationValuePreservedByMCM)
-	return nil
-}
+//// annotateMachineForAutoPreservation annotates the given machine with the auto-preservation annotation to trigger
+//// preservation of the machine by the machine controller.
+//func (c *controller) annotateMachineForAutoPreservation(ctx context.Context, m *v1alpha1.Machine) error {
+//	_, err := UpdateMachineWithRetries(ctx, c.controlMachineClient.Machines(m.Namespace), c.machineLister, m.Namespace, m.Name, func(clone *v1alpha1.Machine) error {
+//		if clone.Annotations == nil {
+//			clone.Annotations = make(map[string]string)
+//		}
+//		clone.Annotations[machineutils.PreserveMachineAnnotationKey] = machineutils.PreserveMachineAnnotationValuePreservedByMCM
+//		return nil
+//	})
+//	if err != nil {
+//		return err
+//	}
+//	klog.V(2).Infof("Updated machine %q with %q=%q.", m.Name, machineutils.PreserveMachineAnnotationKey, machineutils.PreserveMachineAnnotationValuePreservedByMCM)
+//	return nil
+//}
