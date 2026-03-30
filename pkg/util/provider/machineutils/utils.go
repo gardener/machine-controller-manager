8b862f91dca6a2ad7eb24913e82255a6c6c3f0b6
Vivek Ram <55849888+r4mek@users.noreply.github.com>
2026-03-30 13:34:46 +0530

    Fixing the issue where a rapid scale up and scale down can result in a cordoned machine in the cluster. (#1087)
    * checkpoint
    
    * adding unit tests
    
    * fixing tests
    
    * fixing linter errors
    
    * linter fix
    
    * nits
    
    * addressing review comments
    
    * nit
    
    * lint checks
    
    * linter fixes
    
    * addressing review comments part1
    
    * fixing tests
    
    * addressing review comments part2
    
    * addressing review comments part3
    
    * addressing review comments part4
    
    * addressing review comments part5
    
    * adding tests
    
    * linter fix
    
    * addressing review comments part6
    
    * addressing review comments part7
    
    * addressing review comments part8
    
    * addressing review comments part9
    
    * nit
    
    * nit
    
    * addressing review comments part10
    
    * fixing tests
    
    * handling mixed annotation of type [M1,M2~T2,]
    
    * addressing review comments part11
    
    * addressing review comments part12
    
    * adding TODOs to be taken up later

--- pkg/util/provider/machineutils/utils.go
+++ pkg/util/provider/machineutils/utils.go
@@ -59,9 +59,17 @@ const (
 	// NotManagedByMCM annotation helps in identifying the nodes which are not handled by MCM
 	NotManagedByMCM = "node.machine.sapcloud.io/not-managed-by-mcm"
 
-	// TriggerDeletionByMCM can be set on the MachineDeployment and contains the machine names for which deletion should be triggered.
-	// This feature is leveraged by the CA-MCM cloud provider.
+	// TriggerDeletionByMCM is the annotation set on the MachineDeployment by the CA-MCM cloud provider. It contains the machine names
+	// for which deletion should be triggered along with the time when CA decided to scale-down those machines.
+	// Expected format for this annotation value is [M1~T1,M2~T2,...]
 	TriggerDeletionByMCM = "node.machine.sapcloud.io/trigger-deletion-by-mcm"
+
+	// MarkedForDeletionTime is the annotation used to specify the time when machine was marked for deletion.
+	// This is used by MCS to delete the machines which were marked for deletion before the MCS saw the replica change.
+	MarkedForDeletionTime = "machine.sapcloud.io/marked-for-deletion-time"
+
+	// LastDeploymentReplicaChangeByScalerTime is the annotation used to specify the time when machineDeployment replica change was triggered by a scaler.
+	LastDeploymentReplicaChangeByScalerTime = "machine.sapcloud.io/last-deployment-replica-change-by-scaler-time"
 
 	// NodeUnhealthy is a node termination reason for failed machines
 	NodeUnhealthy = "Unhealthy"
