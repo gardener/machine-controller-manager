e632571e93879b210f8988a9fecc9209cb86aa99
Prashant Tak <prashant.tak@sap.com>
2026-07-31 18:55:42 +0530

    Add simulated provider for IT testing without CSP
    Signed-off-by: Prashant Tak <prashant.tak@sap.com>

---
+++ pkg/simulatedprovider/cmd/machine-controller/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	simprovider "github.com/gardener/machine-controller-manager/pkg/simulatedprovider/provider"
+
+	_ "github.com/gardener/machine-controller-manager/pkg/util/client/metrics/prometheus" // for client metric registration
+	"github.com/gardener/machine-controller-manager/pkg/util/provider/app"
+	"github.com/gardener/machine-controller-manager/pkg/util/provider/app/options"
+	_ "github.com/gardener/machine-controller-manager/pkg/util/reflector/prometheus" // for reflector metric registration
+	_ "github.com/gardener/machine-controller-manager/pkg/util/workqueue/prometheus" // for workqueue metric registration
+	"github.com/spf13/pflag"
+	"k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/logs"
+)
+
+func main() {
+
+	s := options.NewMCServer()
+	s.AddFlags(pflag.CommandLine)
+
+	flag.InitFlags()
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if s.TargetKubeconfig == "" {
+		_ = fmt.Errorf("--target-kubeconfig must be provided")
+		os.Exit(1)
+	}
+
+	driver, err := simprovider.NewDriver(context.Background(), s.TargetKubeconfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(2)
+	}
+
+	if err := app.Run(s, driver); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+}
