7d4a186c6f2f3d62d8c9b92e43767156c0e920be
Prashant Tak <prashant.tak@sap.com>
2026-08-25 12:17:39 +0530

    Fix the machine class name in managedNodes
    Signed-off-by: Prashant Tak <prashant.tak@sap.com>

--- pkg/simulatedprovider/provider/node.go
+++ pkg/simulatedprovider/provider/node.go
@@ -34,7 +34,7 @@ func (d *DriverImpl) initializeMCMManagedNodes(ctx context.Context) error {
 	for _, n := range nodeList.Items {
 		d.managedNodes.Store(n.Name, managedNodeInfo{
 			ProviderID:       n.Spec.ProviderID,
-			MachineClassName: n.Name,
+			MachineClassName: n.Labels[labelMachineClassName],
 		})
 	}
 	return nil
